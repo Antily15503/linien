003c5cd711bec8a390cda70394294b8c676ddafc
vedaant <vedaant.n@gmail.com>
2026-06-19 14:01:46 -0700

    fixed typo

--- gateware/logic/sequence.py
+++ gateware/logic/sequence.py
@@ -100,7 +100,7 @@ class SequenceExecutor(Module, AutoCSR):
             self.status_4.status.eq(ttl.o_status_4),
             self.active.eq(ttl.o_active),
             #TODO: pid_pause should be OR'd o_active
-            self.pid_pause.eq((ttl.o_active!=0))
+            self.pid_pause.eq(ttl.o_active!=0),
             self.dac_out.eq(o_dac_drive),
             # snapshot readback
             self.saved_pid_out.status.eq(ttl.o_saved_pid_out),
