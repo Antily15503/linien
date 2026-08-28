9788d61996d81dabce85bef83580ec1ddb38fe23
vedaant <vedaant.n@gmail.com>
2026-08-28 16:18:57 -0400

    branch to test if upward sweep (post ttl, starts from bottom, triggers, sweeps up) works better. created due to autolock.py only allowing a lock to occur on the upward sweep

--- gateware/logic/sweep.py
+++ gateware/logic/sweep.py
@@ -54,8 +54,8 @@ class Sweep(Module):
                 (self.turn & self.up) | (~self.sequence_stop & self.sequence_stop_reg)
             ),
             turning.eq(self.turn),
-            If(self.sequence_stop, dir.eq(0)).Else(dir.eq(self.up)),
-            If(self.sequence_stop, (self.y.eq(self.max)))
+            If(self.sequence_stop, dir.eq(1)).Else(dir.eq(self.up)),
+            If(self.sequence_stop, (self.y.eq(self.min)))
             .Elif((~self.run), self.y.eq(0))
             .Elif(
                 ~self.hold,
